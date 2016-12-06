01183d0ecf30d0bb84829d86f527334f606de08f
Yudaeh <blancveni@gmail.com>
2016-12-06 10:23:37 +0100

    Add files via upload
    CSV finis

--- GestionAbsences/app/Controleur/CSV.php
+++ GestionAbsences/app/Controleur/CSV.php
@@ -5,7 +5,10 @@
 
 
     use GestionAbsences\Core\Controleur;
+    use GestionAbsences\Modele\Adresse;
     use GestionAbsences\Modele\Etudiant;
+    use GestionAbsences\Modele\Pays;
+    use GestionAbsences\Modele\Ville;
 
     class CSV extends Controleur {
 
@@ -13,14 +16,33 @@
          * Méthode lancée par défaut sur un contrôleur.
          */
         public function index() {
-            // TODO: Implement index() method.
+            $this->importEtudiant('patron.csv', 1);
         }
 
-        public function importEtudiant($fichier,$ID_G){
-            $tabR=array();
-            while(($eleve=fgetcsv($fichier))!=false) {
-                array_push($tabR, $eleve);
-                $newEleve = new Etudiant($tabR[0],$tabR[1],$tabR[2],$ID_G);
+        public function importEtudiant($fichier, $ID_G) {
+
+            ini_set('auto_detect_line_endings', true);
+            $fich = fopen($fichier, 'r');
+            while (($tabR = fgetcsv($fich, 0, ";")) != false) {
+
+                echo $tabR[8];
+                $pays = new Pays(0, $tabR[8]);
+                $pays->recherche();
+                echo $pays->toString() . "<br/>";
+                $ville = new Ville(0,$tabR[7],$tabR[6],$pays->getIDP());
+                $ville->recherche();
+                #var_dump($ville);
+                echo $ville->toString()."<br/>";
+                $adresse = new Adresse(0,$tabR[5],$tabR[4],$ville->getIDV());
+                $adresse->recherche();
+                echo $adresse->toString()."<br/>";
+                $newEleve = new Etudiant($tabR[0],$tabR[1],$tabR[2],$ID_G,$tabR[3],$adresse->getIDA(),$tabR[9]);
+                $newEleve->sauvegarder();
+                echo $newEleve->toString();
+
             }
+            ini_set('auto_detect_line_endings', false);
+            fclose($fich);
+
         }
     }
